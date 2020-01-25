b83cb9aa16dbd85635b2da5aa8fe6c8450c85241
Nke0628 <ooo_diva@yahoo.co.jp>
2020-01-25 05:37:36 +0000

    メール送信のテスト #1

---
+++ resources/views/emails/sample_notification_plane.blade.php
@@ -0,0 +1,8 @@
+Sample Notification!
+
+A sample notification has been sent.
+
+{{$text}}
+{{$public_variable_test}}
+
+https://www.google.co.jp
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('sample/mailable/send', 'SampleController@SampleNotification');
